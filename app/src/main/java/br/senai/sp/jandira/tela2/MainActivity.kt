d6e16b13a0e2a97fb9b9812a60a48e4b2a137df8
RUAN CALSOLARI ALMEIDA <23111764@dsn127m1247443.cfp127aluno.local>
2024-03-14 17:08:31 -0300

    Falta a 3 tela

---
+++ app/src/main/java/br/senai/sp/jandira/tela2/MainActivity.kt
@@ -0,0 +1,516 @@
+package br.senai.sp.jandira.tela2
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
+import androidx.activity.ComponentActivity
+import androidx.activity.R
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.tela2.ui.theme.Tela2Theme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Tela2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Login() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            horizontalArrangement = Arrangement.End
+        ){
+            Box(modifier = Modifier
+                .width(120.dp)
+                .height(40.dp)
+                .background(
+                    color = Color(0xFFCF06F0),
+                    shape = RoundedCornerShape(
+                        bottomStart = 16.dp
+                    )
+                )
+            ){}
+        }
+
+
+        Column (modifier = Modifier
+            .padding(22.dp)
+        ){
+            Text(text = "Login",
+                color = Color(0xffCF06F0),
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(text = "Please sign in to continue.",
+                color = Color(0xFFA09C9C)
+            )
+        }
+
+        // color = card defuls.card.color
+
+
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        ){
+
+            OutlinedTextField(
+                value = "teste@email.com ",
+                onValueChange = {},
+                label = {
+                    Text(text = "E-mail")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Lock,
+                        contentDescription = "Lock",
+                        tint = Color(0xffCF06F0),
+                    )
+                },
+                colors = OutlinedTextFieldDefaults
+                    .colors(
+                        focusedBorderColor = Color(0xffCF06F0),
+                        unfocusedBorderColor = Color(0xffCF06F0),
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .width(327.dp)
+            )
+
+            OutlinedTextField(
+                value = "************",
+                onValueChange = {},
+                label = {
+                    Text(text = "Password")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Email,
+                        contentDescription = "Email",
+                        tint = Color(0xffCF06F0),)
+                },
+                colors = OutlinedTextFieldDefaults
+                    .colors(
+                        focusedBorderColor = Color(0xffCF06F0),
+                        unfocusedBorderColor = Color(0xffCF06F0)
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .width(327.dp)
+            )
+
+
+        }
+
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(170.dp)
+                .padding(28.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.End
+        ){
+            Button(onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color((0xffCF06F0)))
+            ) {
+                Row (
+                    modifier = Modifier
+                        .width(134.dp)
+                        .height(48.dp)
+                    ,
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ){
+                    Text(text = "Sign in",
+                        fontSize = 26.sp,
+                        color = Color.White                       )
+                    Icon(imageVector = Icons.Default.ArrowForward,
+                        contentDescription = "oi",
+                        tint = Color(0xFFFFFFFF),
+                    )
+                }
+            }
+            Row {
+                Text(text = "Don’t have an account?",
+                    color = Color(0xFFFA09C9C))
+                Text(text = "Sign up",
+                    color = Color(0xFFCF06F0),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(start = 6.dp)
+                )
+            }
+        }
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom
+        ){
+            Box(modifier = Modifier
+                .width(120.dp)
+                .height(40.dp)
+                .background(
+                    color = Color(0xFFCF06F0),
+                    shape = RoundedCornerShape(
+                        topEnd = 16.dp
+                    )
+                )
+            ){}
+        }
+    }
+}
+
+@Composable
+fun Registro() {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            Box(modifier = Modifier
+                .width(120.dp)
+                .height(40.dp)
+                .background(
+                    color = Color(0xFFCF06F0),
+                    shape = RoundedCornerShape(
+                        bottomStart = 16.dp
+                    )
+                )
+            ){}
+        }
+
+        Column (modifier = Modifier
+            .fillMaxWidth()
+            .padding(22.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Text(text = "Sign Up",
+                color = Color(0xffCF06F0),
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(text = "Create a new account",
+                color = Color(0xFFA09C9C)
+            )
+        }
+
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(160.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+        ) {
+
+            Box(modifier = Modifier
+                .width(100.dp)
+                .height(100.dp),
+            ){
+                Box(modifier = Modifier){
+                    Image(
+                        painter = painterResource(id = br.senai.sp.jandira.tela2.R.drawable.eclipse),
+                        contentDescription = "",
+                        modifier = Modifier.size(100.dp),
+
+                        )
+                }
+
+                Icon(imageVector = Icons.Default.Person,
+                    contentDescription = "Person",
+                    tint = Color(0xffCF06F0),
+                    modifier = Modifier
+                        .size(90.dp)
+                        .offset(y = 0.dp, x = 5.dp)
+                )
+
+                Image(
+                    painter = painterResource(id = br.senai.sp.jandira.tela2.R.drawable.vectorcam),
+                    contentDescription = "Mudar foto de perfil",
+                    modifier = Modifier
+                        .size(28.dp)
+                        .offset(y = 80.dp, x = 80.dp)
+                )
+
+            }
+        }
+
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+        ){
+
+
+
+
+            OutlinedTextField(
+                value = "Susanna Hoffs",
+                onValueChange = {},
+                label = {
+                    Text(text = "Username")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Person,
+                        contentDescription = "Person",
+                        tint = Color(0xffCF06F0),
+                    )
+                },
+                colors = OutlinedTextFieldDefaults
+                    .colors(
+                        focusedBorderColor = Color(0xffCF06F0),
+                        unfocusedBorderColor = Color(0xffCF06F0),
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .width(327.dp)
+            )
+
+            OutlinedTextField(
+                value = "99999-0987",
+                onValueChange = {},
+                label = {
+                    Text(text = "Phone")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Phone,
+                        contentDescription = "Phone",
+                        tint = Color(0xffCF06F0),)
+                },
+                colors = OutlinedTextFieldDefaults
+                    .colors(
+                        focusedBorderColor = Color(0xffCF06F0),
+                        unfocusedBorderColor = Color(0xffCF06F0)
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .width(327.dp)
+            )
+
+            OutlinedTextField(
+                value = "susanna@email.com ",
+                onValueChange = {},
+                label = {
+                    Text(text = "E-mail")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Email,
+                        contentDescription = "Email",
+                        tint = Color(0xffCF06F0),)
+                },
+                colors = OutlinedTextFieldDefaults
+                    .colors(
+                        focusedBorderColor = Color(0xffCF06F0),
+                        unfocusedBorderColor = Color(0xffCF06F0)
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .width(327.dp)
+            )
+
+            OutlinedTextField(
+                value = "************",
+                onValueChange = {},
+                label = {
+                    Text(text = "Password")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Lock,
+                        contentDescription = "Password",
+                        tint = Color(0xffCF06F0),)
+                },
+                colors = OutlinedTextFieldDefaults
+                    .colors(
+                        focusedBorderColor = Color(0xffCF06F0),
+                        unfocusedBorderColor = Color(0xffCF06F0)
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .width(327.dp)
+            )
+
+        }
+        Row (
+            modifier = Modifier
+                .width(200.dp)
+                .height(50.dp)
+                .padding(start = 37.dp),
+            verticalAlignment = Alignment.CenterVertically,
+
+            ){
+
+
+            Checkbox(
+                checked = false,
+                onCheckedChange  = {},
+                colors = CheckboxDefaults
+                    .colors(
+                        checkedColor = Color(0xffCF06F0),
+                        uncheckedColor = Color(0xffCF06F0)
+                    )
+            )
+            Text(text = "Over 18?")
+        }
+
+        Column (
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Button(onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color((0xffCF06F0)))
+            ) {
+                Row(
+                    modifier = Modifier
+                        .width(295.dp)
+                        .height(48.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    Text(
+                        color = Color.White,
+                        text = "Create Account",
+                        fontSize = 26.sp
+                    )
+                }
+            }
+        }
+
+        Row (modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(end = 32.dp),
+            horizontalArrangement = Arrangement.End
+
+        ){
+            Text(text = "Already have an account?",
+                color = Color(0xFFFA09C9C))
+            Text(text = "Login",
+                color = Color(0xFFCF06F0),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 6.dp)
+            )
+        }
+    }
+
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.Bottom
+    ){
+        Box(modifier = Modifier
+            .width(120.dp)
+            .height(40.dp)
+            .background(
+                color = Color(0xFFCF06F0),
+                shape = RoundedCornerShape(
+                    topEnd = 16.dp
+                )
+            )
+        ){}
+    }
+
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    Tela2Theme {
+        Login()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RegistroPreview() {
+    Tela2Theme {
+        Registro()
+    }
+}
